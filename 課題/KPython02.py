031b583d4f280301a66982f514f78a2a6c1af8c6
kagaya25 <jm.kagaya@gmail.com>
2018-05-15 09:45:48 +0900

    Done
    Done

--- 課題/KPython02.py
+++ 課題/KPython02.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Python 3
-0H01007 加賀屋　ジャンメデルPython 3
 0H01007 加賀屋　ジャンメデル
+
 """
 print ("IT情報処理学科、ソフトⅡコース１組の加賀屋ジャンメデルです。")
 print ("今年は何年ですか? ")
 year = input()
 year_int = int(year)
-print ("1993年生まれなので、", year_int - 1998, "歳になります。")
+print ("1993年生まれなので、", year_int - 1993, "歳になります。")
