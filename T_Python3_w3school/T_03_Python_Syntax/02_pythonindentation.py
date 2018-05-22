a37b737c6236733a6c3304cd8838312514950643
kagaya25 <jm.kagaya@gmail.com>
2018-05-22 11:03:47 +0900

    Done
    Done

---
+++ T_Python3_w3school/T_03_Python_Syntax/02_pythonindentation.py
@@ -0,0 +1,9 @@
+
+
+#Python Indentations
+#Where in other programming languages the indentation in code is for readability only, in Python the indentation is very important.
+#Python uses indentation to indicate a block of code.
+
+if 5 > 2:
+    print("Five is greater than two!")
+    
---
+++ T_Python3_w3school/T_03_Python_Syntax/03_python_Indentation_Error.py
@@ -0,0 +1,7 @@
+
+
+
+#Python will give you an error if you skip the indentation:
+
+if 5 > 2:
+print("Five is greater than two!")
