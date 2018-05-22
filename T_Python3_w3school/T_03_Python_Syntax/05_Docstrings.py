019926a4d4adae9c892ae0855eaad2a4f458abc8
kagaya25 <jm.kagaya@gmail.com>
2018-05-22 11:04:39 +0900

    Done
    Done

--- T_Python3_w3school/T_03_Python_Syntax/05_Docstrings.py
+++ T_Python3_w3school/T_03_Python_Syntax/05_Docstrings.py
@@ -1,3 +1,14 @@
 
 
+"""
+Docstrings
+Python also has extended documentation capability, called docstrings.
 
+Docstrings can be one line, or multiline.
+
+Python uses triple quotes at the beginning and end of the docstring:"""
+
+
+"""This is a
+multiline docstring."""
+print("Hello, World!")
