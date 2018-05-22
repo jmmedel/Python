fe98e88f50bb6066e9f7d35c2981431c14b02f94
kagaya25 <jm.kagaya@gmail.com>
2018-05-22 11:11:07 +0900

    Done
    Done

---
+++ T_Python3_w3school/T_04_Python_Variables/01_Variable.py
@@ -0,0 +1,13 @@
+
+"""
+Python Variables
+Creating Variables
+Unlike other programming languages, Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it."""
+
+
+x = 5
+y = "john"
+print(x)
+print(y)
