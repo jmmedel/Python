c5a4b7d686aceba4e4c9757b9215815160c8a60a
kagaya25 <jm.kagaya@gmail.com>
2018-05-24 10:35:25 +0900

    Done
    Done

--- References/02_T_Python3_Tutorialpoint/11_Loop/04_for.py
+++ References/02_T_Python3_Tutorialpoint/11_Loop/04_for.py
@@ -8,3 +8,25 @@ Syntax
 for iterating_var in sequence:
    statements(s)
 If a sequence contains an expression list, it is evaluated first. Then, the first item in the sequence is assigned to the iterating variable iterating_var. Next, the statements block is executed. Each item in the list is assigned to iterating_var, and the statement(s) block is executed until the entire sequence is exhausted."""
+
+"""
+The range() function
+The built-in function range() is the right function to iterate over a sequence of numbers. It generates an iterator of arithmetic progressions.
+
+Example
+Example
+range() generates an iterator to progress integers starting with 0 upto n-1. To obtain a list object of the sequence, it is typecasted to list(). Now this list can be iterated using the for statement."""
+list(range(5))
+for var in list(range(5)):
+    print(var)
+print()
+
+for letter in "Python":
+    print("Current Letter :",letter)
+print()
+
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # traversal of List sequence
+   print ('Current fruit :', fruit)
+
+print ("Good bye!")
---
+++ References/02_T_Python3_Tutorialpoint/11_Loop/05_for_iterating_index.py
@@ -0,0 +1,15 @@
+
+
+"""
+Iterating by Sequence Index
+An alternative way of iterating through each item is by index offset into the sequence itself. Following is a simple example −"""
+
+fruits = ['banana', 'apple',  'mango']
+for index in range(len(fruits)):
+   print ('Current fruit :', fruits[index])
+
+print ("Good bye!")
+
+
+"""
+Here, we took the assistance of the len() built-in function, which provides the total number of elements in the tuple as well as the range() built-in function to give us the actual sequence to iterate over."""
