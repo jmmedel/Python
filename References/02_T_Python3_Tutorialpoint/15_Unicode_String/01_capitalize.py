741293b6d8675ee52840b8aeda2d95478b10e054
kagaya25 <jm.kagaya@gmail.com>
2018-05-24 11:20:53 +0900

    Done
    Done

---
+++ References/02_T_Python3_Tutorialpoint/15_Unicode_String/01_capitalize.py
@@ -0,0 +1,21 @@
+"""
+Python 3 - String capitalize() Method
+Advertisements
+ Previous Page Next Page  
+It returns a copy of the string with only its first character capitalized.
+
+Syntax
+Following is the syntax for capitalize() method −
+
+str.capitalize()
+Parameters
+NA
+
+Return Value
+string
+
+Example"""
+
+str = "this is string example....wow!!!"
+
+print ("str.capitalize() : ", str.capitalize())
