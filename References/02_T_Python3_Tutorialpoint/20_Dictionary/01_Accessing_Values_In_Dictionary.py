9b4560e5bc443a84c81a9f06f2027ff028c02dfd
kagaya25 <jm.kagaya@gmail.com>
2018-05-25 12:09:00 +0900

    Done
    Done

---
+++ References/02_T_Python3_Tutorialpoint/20_Dictionary/01_Accessing_Values_In_Dictionary.py
@@ -0,0 +1,26 @@
+
+
+
+"""
+Python 3 - Dictionary
+Python 3 - Dictionary
+
+Each key is separated from its value by a colon (:), the items are separated by commas, and the whole thing is enclosed in curly braces. An empty dictionary without any items is written with just two curly braces, like this: {}.
+
+Keys are unique within a dictionary while values may not be. The values of a dictionary can be of any type, but the keys must be of an immutable data type such as strings, numbers, or tuples.
+
+Accessing Values in Dictionary
+To access dictionary elements, you can use the familiar square brackets along with the key to obtain its value. Following is a simple example −"""
+
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print ("dict['Name']: ", dict['Name'])
+print ("dict['Age']: ", dict['Age'])
+
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+print "dict['Alice']: ", dict['Alice']
+
+
+
+
