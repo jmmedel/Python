718b7edb94a161c11d2f8bb9e57483fcd4a66ea8
kagaya25 <jm.kagaya@gmail.com>
2018-05-29 23:56:47 +0900

    Done
    Done

--- References/02_T_Python3_Tutorialpoint/29_Exception_Handling/10_User_Defined_Exceptions.py
+++ References/02_T_Python3_Tutorialpoint/29_Exception_Handling/10_User_Defined_Exceptions.py
@@ -11,4 +11,14 @@ Here is an example related to RuntimeError. Here, a class is created that is sub
 In the try block, the user-defined exception is raised and caught in the except block. The variable e is used to create an instance of the class Networkerror.
 
 
+class Networkerror(RuntimeError):
+   def __init__(self, arg):
+      self.args = arg
+So once you have defined the above class, you can raise the exception as follows −
+
+try:
+   raise Networkerror("Bad hostname")
+except Networkerror,e:
+   print e.args
+   
 """
