2c2c5d4da8687228498e5ef6de42aab1e6edb732
kagaya25 <jm.kagaya@gmail.com>
2018-05-29 23:25:01 +0900

    Done
    done

---
+++ References/02_T_Python3_Tutorialpoint/36_Sending_Email_Using_SMTP/02_SMTP.py
@@ -0,0 +1,15 @@
+
+
+
+"""
+
+Sending an HTML e-mail using Python
+When you send a text message using Python, then all the content is treated as simple text. Even if you include HTML tags in a text message, it is displayed as simple text and HTML tags will not be formatted according to the HTML syntax. However, Python provides an option to send an HTML message as actual HTML message.
+
+While sending an e-mail message, you can specify a Mime version, content type and the character set to send an HTML e-mail.
+
+Example
+Following is an example to send the HTML content as an e-mail. Try it once −
+
+
+"""
