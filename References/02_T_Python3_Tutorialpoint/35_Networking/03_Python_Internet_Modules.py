b855d34dec450041d448c86165a0775b0ba438ab
kagaya25 <jm.kagaya@gmail.com>
2018-05-29 23:19:02 +0900

    Done
    Done

---
+++ References/02_T_Python3_Tutorialpoint/35_Networking/03_Python_Internet_Modules.py
@@ -0,0 +1,30 @@
+
+
+
+"""
+
+Python Internet Modules
+A list of some important modules in Python Network/Internet programming are given below −
+
+Protocol	Common function	Port No	Python module
+HTTP	Web pages	80	httplib, urllib, xmlrpclib
+NNTP	Usenet news	119	nntplib
+FTP	File transfers	20	ftplib, urllib
+SMTP	Sending email	25	smtplib
+POP3	Fetching email	110	poplib
+IMAP4	Fetching email	143	imaplib
+Telnet	Command lines	23	telnetlib
+Gopher	Document transfers	70	gopherlib, urllib
+Please check all the libraries mentioned above to work with FTP, SMTP, POP, and IMAP protocols.
+
+Further Readings
+This was a quick start with the Socket Programming. It is a vast subject. It is recommended to go through the following link to find more detail −
+
+Unix Socket Programming.
+
+Python Socket Library and Modules.
+
+
+
+
+"""
