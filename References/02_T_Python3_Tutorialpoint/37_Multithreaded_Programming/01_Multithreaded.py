9f3d067d30f3c359801cfb73a88e20f23d2ae3c8
kagaya25 <jm.kagaya@gmail.com>
2018-05-29 23:31:53 +0900

    Done
    Done

---
+++ References/02_T_Python3_Tutorialpoint/37_Multithreaded_Programming/01_Multithreaded.py
@@ -0,0 +1,68 @@
+
+
+
+"""
+
+Python 3 - Multithreaded Programming
+
+Running several threads is similar to running several different programs concurrently, but with the following benefits −
+
+Multiple threads within a process share the same data space with the main thread and can therefore share information or communicate with each other more easily than if they were separate processes.
+
+Threads are sometimes called light-weight processes and they do not require much memory overhead; they are cheaper than processes.
+
+A thread has a beginning, an execution sequence, and a conclusion. It has an instruction pointer that keeps track of where within its context is it currently running.
+
+It can be pre-empted (interrupted).
+
+It can temporarily be put on hold (also known as sleeping) while other threads are running - this is called yielding.
+
+There are two different kind of threads −
+
+kernel thread
+user thread
+Kernel Threads are a part of the operating system, while the User-space threads are not implemented in the kernel.
+
+There are two modules which support the usage of threads in Python3 −
+
+_thread
+threading
+The thread module has been "deprecated" for quite a long time. Users are encouraged to use the threading module instead. Hence, in Python 3, the module "thread" is not available anymore. However, it has been renamed to "_thread" for backwards compatibilities in Python3.
+
+Starting a New Thread
+To spawn another thread, you need to call the following method available in the thread module −
+
+_thread.start_new_thread ( function, args[, kwargs] )
+This method call enables a fast and efficient way to create new threads in both Linux and Windows.
+
+The method call returns immediately and the child thread starts and calls function with the passed list of args. When the function returns, the thread terminates.
+
+Here, args is a tuple of arguments; use an empty tuple to call function without passing any arguments. kwargs is an optional dictionary of keyword arguments.
+
+Example
+
+"""
+
+#!/usr/bin/python3
+
+import _thread
+import time
+
+# Define a function for the thread
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+# Create two threads as follows
+try:
+   _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+   _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+   print ("Error: unable to start thread")
+
+while 1:
+   pass
+   
