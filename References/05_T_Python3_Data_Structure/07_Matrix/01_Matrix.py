4902f7be5eab0fa7be2b7da569a283153eede2d5
kagaya25 <jm.kagaya@gmail.com>
2018-05-31 12:08:31 +0900

    Done
    Done

--- References/05_T_Python3_Data_Structure/07_Matrix/01_Matrix.py
+++ References/05_T_Python3_Data_Structure/07_Matrix/01_Matrix.py
@@ -23,6 +23,21 @@ a = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
 m = reshape(a,(7,5))
 print(m)
 
+"""
+
+The above data can be represented as a two dimensional array as below.
+
+[['Mon' '18' '20' '22' '17']
+ ['Tue' '11' '18' '21' '18']
+ ['Wed' '15' '21' '20' '19']
+ ['Thu' '11' '20' '22' '21']
+ ['Fri' '18' '17' '23' '22']
+ ['Sat' '12' '22' '20' '18']
+ ['Sun' '13' '15' '19' '16']]
+ 
+"""
+
+
 
 
 
