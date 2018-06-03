b90f492a969eeafaf5a901b1c80fe35b34a19b2a
kagaya <jm.kagaya@gmail.com>
2018-06-03 10:28:48 +0900

    Done

--- References/05_T_Python3_Data_Structure/08_Set/03_Set.py
+++ References/05_T_Python3_Data_Structure/08_Set/03_Set.py
@@ -11,5 +11,21 @@ We can add elements to a set by using add() method. Again as discussed there is 
 
 """
 
+Days=set(["Mon","Tue","Wed","Thu","Fri","Sat"])
+ 
+Days.add("Sun")
+print(Days)
+
+
+"""
+
+When the above code is executed, it produces the following result.
+
+set(['Wed', 'Sun', 'Fri', 'Tue', 'Mon', 'Thu', 'Sat'])
+
+"""
+
+
+
 
 
