84d071b59340f3cb2730f5f2dc6ff7f0184543c0
kagaya <jm.kagaya@gmail.com>
2018-06-03 10:22:40 +0900

    Done

---
+++ References/05_T_Python3_Data_Structure/08_Set/03_Set.py
@@ -0,0 +1,15 @@
+
+
+
+
+"""
+
+
+Adding Items to a Set
+
+We can add elements to a set by using add() method. Again as discussed there is no specific index attached to the newly added element.
+
+"""
+
+
+
