e4f3c02567205e8368695b99ca5944d6940ae44b
kjmkagaya <jm.kagaya@gmail.com>
2018-06-06 14:12:01 +0900

    Done
    Done

--- References/11_Python_Pandas/06_Basic_Functionality/01_Basic_Fucntionality.py
+++ References/11_Python_Pandas/06_Basic_Functionality/01_Basic_Fucntionality.py
@@ -6,5 +6,42 @@
 
 Python Pandas - Basic Functionality
 
+By now, we learnt about the three Pandas DataStructures and how to create them. We will majorly focus on the DataFrame objects because of its importance in the real time data processing and also discuss a few other DataStructures.
+
+Series Basic Functionality
+S.No.	Attribute or Method	Description
+1	axes	Returns a list of the row axis labels.
+2	dtype	Returns the dtype of the object.
+3	empty	Returns True if series is empty.
+4	ndim	Returns the number of dimensions of the underlying data, by definition 1.
+5	size	Returns the number of elements in the underlying data.
+6	values	Returns the Series as ndarray.
+7	head()	Returns the first n rows.
+8	tail()	Returns the last n rows.
+Let us now create a Series and see all the above tabulated attributes operation.
+
+Example
 
 """
+
+import pandas as pd
+import numpy as np
+
+#Create a series with 100 random numbers
+s = pd.Series(np.random.randn(4))
+print (s)
+
+
+"""
+
+Its output is as follows −
+
+0   0.967853
+1  -0.148368
+2  -1.395906
+3  -1.758394
+dtype: float64
+
+
+"""
+
--- References/11_Python_Pandas/06_Basic_Functionality/02_Basic_Fucntionality.py
+++ References/11_Python_Pandas/06_Basic_Functionality/02_Basic_Fucntionality.py
@@ -1,1 +1,39 @@
-p
+
+
+"""
+
+axes
+Returns the list of the labels of the series.
+
+
+"""
+
+
+import pandas as  pd
+import numpy as np 
+
+#Create a series with 4 random numbers
+
+#Create a series with 4 random numbers
+s = pd.Series(np.random.randn(4))
+print (s)
+
+print ("The actual data series is:")
+print (s.values)
+
+"""
+
+Its output is as follows −
+
+0   1.787373
+1  -0.605159
+2   0.180477
+3  -0.140922
+dtype: float64
+
+The actual data series is:
+[ 1.78737302 -0.60515881 0.18047664 -0.1409218 ]
+
+
+"""
+
