df94ade56342f2991a67ed8aeea3d97fbfb52862
kagaya <jm.kagaya@gmail.com>
2018-06-07 09:36:59 +0900

    done

---
+++ References/11_Python_Pandas/07_Descriptive Statistics/01_python.py
@@ -0,0 +1,32 @@
+
+
+"""
+Python Pandas - Descriptive Statistics
+
+A large number of methods collectively compute descriptive statistics and other related operations on DataFrame. Most of these are aggregations like sum(), mean(), but some of them, like sumsum(), produce an object of the same size. Generally speaking, these methods take an axis argument, just like ndarray.{sum, std, ...}, but the axis can be specified by name or integer
+
+DataFrame − “index” (axis=0, default), “columns” (axis=1)
+
+Let us create a DataFrame and use this object throughout this chapter for all the operations.
+
+Example
+
+
+"""
+
+import pandas as pd
+import numpy as np
+
+#Create a Dictionary of series
+d = {'Name':pd.Series(['Tom','James','Ricky','Vin','Steve','Smith','Jack',
+   'Lee','David','Gasper','Betina','Andres']),
+   'Age':pd.Series([25,26,25,23,30,29,23,34,40,30,51,46]),
+   'Rating':pd.Series([4.23,3.24,3.98,2.56,3.20,4.6,3.8,3.78,2.98,4.80,4.10,3.65])}
+
+#Create a DataFrame
+df = pd.DataFrame(d)
+print (df)
+
+
+
+
